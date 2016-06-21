76f8c4967b8041b4994c3180538edc16eaa5cb21
Viktor Dorozsmai <vikdoro@gmail.com>
2016-06-21 14:29:00 +0100

    before checkout earlier map

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -27,10 +27,16 @@ $(document).ready(function(){
     google.maps.event.trigger(inputMap, "resize");
     inputMap.setCenter(new google.maps.LatLng(51.506178,-0.088369)); 
   });
+
+
+
+
   }
 
 function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('address').value;
+    var searchBox = document.getElementById('address');
+    var autocomplete = new google.maps.places.Autocomplete(searchBox);
     geocoder.geocode({'address': address}, function(results, status) {
       if (status === google.maps.GeocoderStatus.OK) {
         resultsMap.setCenter(results[0].geometry.location);
@@ -47,6 +53,10 @@ function geocodeAddress(geocoder, resultsMap) {
     });
   }
 
+
+
+
+
 // initialise map
   var canvas = document.getElementById("map-canvas");
 
@@ -56,6 +66,8 @@ function geocodeAddress(geocoder, resultsMap) {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
         styles: [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
   }
+
+
 
 
 var map = new google.maps.Map(canvas , mapOptions);
