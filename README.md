3f572deabafe4585f55aa9a997f9c3ea4c9c4fab
Alissa Schlecht <schlecht220@gmail.com>
2016-06-22 15:53:10 +0100

    remerge of saved work

---
+++ README.md
@@ -0,0 +1,56 @@
+# WDI_20_PROJECT_3
+Team Shandy
+Merge test
+Group project - Alissa, Gus, Viktor
+
+
+### Wireframe
+
+mybalbsamiq.com
+
+web app to access api
+-login users 
+-crud pages
+
+
+### Wireframe end
+
+This is a one week project using node.js and api's. 
+
+
+### User Stories
+-------------------------------------------
+
+See full user stories here: 
+
+
+Users should be able to: 
+- 
+-
+-
+-
+-
+-
+-
+-
+-
+-
+
+
+Added a gus branch using git branch
+
+---------------------------
+
+Intro 
+-----------------
+
+This project aims to do three things. 
+
+    1. Populate googlemaps using user created data. 
+
+    2. The data should be about upcoming gigs and give their location.
+
+    3. Users should be able to view gigs on a given day and in a given area, wiht the ability to see the band's website. 
+
+
+    
---
+++ api/app.js
@@ -0,0 +1,34 @@
+var express       = require('express');
+var app           = express();
+var morgan        = require('morgan');
+var port           = process.env.PORT || 3000;
+var mongoose       = require('mongoose');
+var methodOverride = require('method-override');
+var bodyParser     = require('body-parser');
+var cors           = require('cors');
+var routes         = require('./config/routes');
+
+
+mongoose.connect('mongodb://localhost:27017/lineup');
+
+// MIDDLEWARE
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === "object" && "_method" in req.body){
+    var method = req.body._method;
+    delete req.body._method;
+    return method; 
+  }
+}));
+app.use(routes);
+
+app.listen(port, function() {
+  console.log("Express is listening on port " + port);
+});
+
+
+
+
